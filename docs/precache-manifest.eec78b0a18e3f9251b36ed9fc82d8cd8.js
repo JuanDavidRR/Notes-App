e3bbaf084b3fd59ef27d5b1c2d7ed5f45c9c26e9
David Romero <juandavidrom@outlook.com>
2021-11-07 22:17:13 -0500

    Corrigiendo docs

---
+++ docs/precache-manifest.eec78b0a18e3f9251b36ed9fc82d8cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e62e53d1a11c272fd5087b02fd5cded",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8783db2bd28e974b551",
+    "url": "/static/css/main.8aa7c4dc.chunk.css"
+  },
+  {
+    "revision": "2f439992719be2bcf4bc",
+    "url": "/static/js/2.b65d8660.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.b65d8660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8783db2bd28e974b551",
+    "url": "/static/js/main.6a53a605.chunk.js"
+  },
+  {
+    "revision": "6e76b1754a6e6c329ffb",
+    "url": "/static/js/runtime-main.50fb5f2f.js"
+  }
+]);
